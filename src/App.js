cb071f8a9014fe67333e2a822f796bf6877d7d79
sk ashfak <skashfak18@gmail.com>
2023-07-18 10:43:32 +0530

    Paralax_web

---
+++ src/App.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import './App.css';
+import Paralax from "./Paralax Web/Paralax"
+import "./Paralax Web/Paralax.css"
+
+
+
+
+
+
+
+
+function App() {
+
+
+  return (
+    <div className="App">
+    <Paralax/>
+    </div>
+
+  )
+}
+
+
+
+export default App;
---
+++ src/Paralax Web/Paralax.jsx
@@ -0,0 +1,60 @@
+import React from 'react'
+
+export default function Paralax() {
+  return (
+    <>
+      <section>
+        <div className="pimg1">
+            <div className="ptext">
+                <span className='text_bg'>
+                    Paralax Website
+                </span>
+            </div>
+        </div>
+        <div className="p-section1">
+            <h1> Section One</h1>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptate expedita officiis eum aut alias modi
+              , cumque et. Illo, maiores tempora? Aliquid omnis harum laboriosam nemo officia minus nostrum neque quibusdam consectetur nulla sapiente exercitationem nesciunt pariatur nam, repellat at odit quis maiores aut fuga? Sunt commodi debitis, minus laudantium recusandae reiciendis possimus voluptatem officiis eligendi unde temporibus facere maiores iure mollitia, repellendus libero dolorem odit ipsam dolorum officia consequuntur ullam. Et voluptas cum quo, fugiat, sunt assumenda, quidem veritatis totam consectetur vero nulla iure blanditiis dolorum. Labore totam blanditiis repellat sunt, quaerat laudantium tempora aliquam placeat, vitae dignissimos sint eum cum quis. Architecto soluta aut, iure ab pariatur excepturi amet suscipit, repellat id eaque aperiam beatae ipsam deserunt. Unde reprehenderit hic saepe iure optio facere. Iste libero est deserunt expedita eum amet molestias accusamus, fugit quod ut dolorum optio vero aliquam, nam id temporibus in eius.</p>
+        </div>
+
+        <div className="pimg2">
+            <div className="ptext">
+                <span className='text_bg'>
+                   Explore Nature
+                </span>
+            </div>
+        </div>
+
+        <div className="p-section1">
+            <h1> Section Two</h1>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptate expedita officiis eum aut alias modi
+              , cumque et. Illo, maiores tempora? Aliquid omnis harum laboriosam nemo officia minus nostrum neque quibusdam consectetur nulla sapiente exercitationem nesciunt pariatur nam, repellat at odit quis maiores aut fuga? Sunt commodi debitis, minus laudantium recusandae reiciendis possimus voluptatem officiis eligendi unde temporibus facere maiores iure mollitia, repellendus libero dolorem odit ipsam dolorum officia consequuntur ullam. Et voluptas cum quo, fugiat, sunt assumenda, quidem veritatis totam consectetur vero nulla iure blanditiis dolorum. Labore totam blanditiis repellat sunt, quaerat laudantium tempora aliquam placeat, vitae dignissimos sint eum cum quis. Architecto soluta aut, iure ab pariatur excepturi amet suscipit, repellat id eaque aperiam beatae ipsam deserunt. Unde reprehenderit hic saepe iure optio facere. Iste libero est deserunt expedita eum amet molestias accusamus, fugit quod ut dolorum optio vero aliquam, nam id temporibus in eius.</p>
+        </div>
+
+        <div className="pimg3">
+            <div className="ptext">
+                <span className='text_bg1'>
+                   look deep into nature
+                </span>
+            </div>
+        </div>
+
+        <div className="p-section1">
+            <h1> Section Three</h1>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptate expedita officiis eum aut alias modi
+              , cumque et. Illo, maiores tempora? Aliquid omnis harum laboriosam nemo officia minus nostrum neque quibusdam consectetur nulla sapiente exercitationem nesciunt pariatur nam, repellat at odit quis maiores aut fuga? Sunt commodi debitis, minus laudantium recusandae reiciendis possimus voluptatem officiis eligendi unde temporibus facere maiores iure mollitia, repellendus libero dolorem odit ipsam dolorum officia consequuntur ullam. Et voluptas cum quo, fugiat, sunt assumenda, quidem veritatis totam consectetur vero nulla iure blanditiis dolorum. Labore totam blanditiis repellat sunt, quaerat laudantium tempora aliquam placeat, vitae dignissimos sint eum cum quis. Architecto soluta aut, iure ab pariatur excepturi amet suscipit, repellat id eaque aperiam beatae ipsam deserunt. Unde reprehenderit hic saepe iure optio facere. Iste libero est deserunt expedita eum amet molestias accusamus, fugit quod ut dolorum optio vero aliquam, nam id temporibus in eius.</p>
+        </div>
+
+        <div className="pimg4">
+            <div className="ptext">
+                <span className='text_bg'>
+                  do Some Codding
+                </span>
+            </div>
+        </div>
+
+      </section>
+      
+    </>
+  )
+}
